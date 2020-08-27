70a977d50440cd4078b2ebd35b4e022b93c57b5d
Abdelrahman El-Adawy <abdel.eladawy@gmail.com>
2020-08-27 02:51:33 +0200

    create first successful migration

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
-import mikroConfig from './mikro-orm.config'
+import mikroConfig from './mikro-orm.config';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
 
   const post = orm.em.create(Post, { title: 'my first post' });
   await orm.em.persistAndFlush(post);
