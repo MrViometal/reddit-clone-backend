bbf2d6c5064d70346c1b956e129cfd783dc056f5
Abdelrahman El-Adawy <abdel.eladawy@gmail.com>
2020-08-30 12:42:55 +0200

    fixed the server breakings, still need to figure out the cookie

--- dist/resolvers/user.js
+++ dist/resolvers/user.js
@@ -104,12 +104,19 @@ let UserResolver = class UserResolver {
                 };
             }
             const hashedPassword = yield argon2_1.default.hash(options.password);
-            const user = em.create(User_1.User, {
-                username: options.username,
-                password: hashedPassword,
-            });
+            let user;
             try {
-                yield em.persistAndFlush(user);
+                const result = yield em
+                    .createQueryBuilder(User_1.User)
+                    .getKnexQuery()
+                    .insert({
+                    username: options.username,
+                    password: hashedPassword,
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                })
+                    .returning('*');
+                user = result[0];
             }
             catch (err) {
                 if (err.code === '23505') {
@@ -123,7 +130,9 @@ let UserResolver = class UserResolver {
                     };
                 }
             }
-            return { user };
+            return {
+                user: Object.assign(Object.assign({}, user), { createdAt: user.created_at, updatedAt: user.updated_at }),
+            };
         });
     }
     login(options, { em, req }) {
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
 import { MyContext } from 'src/types';
 import { User } from '../entities/User';
 import argon2 from 'argon2';
+import { EntityManager } from '@mikro-orm/postgresql';
 
 @InputType()
 class UsernamePasswordInput {
@@ -87,12 +88,19 @@ export class UserResolver {
       };
     }
     const hashedPassword = await argon2.hash(options.password);
-    const user = em.create(User, {
-      username: options.username,
-      password: hashedPassword,
-    });
+    let user;
     try {
-      await em.persistAndFlush(user);
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning('*');
+      user = result[0];
     } catch (err) {
       if (err.code === '23505') {
         //duplicate username error
@@ -106,7 +114,18 @@ export class UserResolver {
         };
       }
     }
-    return { user };
+    // return { user:{
+    //   ...user,
+    //   createdAt=user.created_at,
+
+    // } };
+    return {
+      user: {
+        ...user,
+        createdAt: user.created_at,
+        updatedAt: user.updated_at,
+      },
+    };
   }
 
   //login a user
