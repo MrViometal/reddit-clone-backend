a40373c287cbb5ce64cda7fad1b9f0c43ed91d5f
Abdelrahman El-Adawy <abdel.eladawy@gmail.com>
2020-08-31 14:33:06 +0200

    server side rendering

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,6 +1,6 @@
-import { Resolver, Query, Ctx, Arg, Mutation } from 'type-graphql';
-import { Post } from '../entities/Post';
 import { MyContext } from 'src/types';
+import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
+import { Post } from '../entities/Post';
 
 //graphql resolver
 @Resolver()
